328c4cc18dbc420b908008892db27e76e0d0fccb
Victor Martins <victor_martins_castro@hotmail.com>
2025-03-18 08:33:01 -0300

    update pnpm-lock.yaml

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  root: 'public',  // Certifique-se de que o root é a raiz
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 3000,
   },
